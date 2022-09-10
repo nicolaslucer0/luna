78d0ca20644986c39eac7ea2747c2ce49cb4c42e
Nicolas Lucero <nicolas.lucero@redb.ee>
2022-09-10 14:28:52 -0300

    changed header

--- Yana/Utils/Utils/NotificacionesManager/TwilioManager.cs
+++ Yana/Utils/Utils/NotificacionesManager/TwilioManager.cs
@@ -11,15 +11,14 @@ namespace Yana.Utils.Utils.NotificacionesManager
             const string accountSid = "AC1be1a20ae3aa2b9b9e5eaace1c477b66";
             const string authToken = "059d6805912407a22513d2a4f157f725";
         
-            TwilioClient.Init(accountSid, authToken);
+            //     TwilioClient.Init(accountSid, authToken);
+            //     var message = MessageResource.Create(
+            //     body: "¿Como estás?. Respondé ingresando a http://www.yana.com.ar/",
+            //     from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
+            //     to: new Twilio.Types.PhoneNumber("whatsapp:+5491141658965")
+            //     );
         
-            var message = MessageResource.Create(
-                body: "¿Como estás?. Respondé ingresando a http://www.yana.com.ar/",
-                from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
-                to: new Twilio.Types.PhoneNumber("whatsapp:+5491141658965")
-            );
-        
-            Console.WriteLine(message.Sid);
+           // Console.WriteLine(message.Sid);
         }
     }
 }
