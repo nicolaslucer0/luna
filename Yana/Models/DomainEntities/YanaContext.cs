534d67b66c80c3e925082f8d7d74bf60bd3473d2
Nicolas Lucero <nicolas.lucero@redb.ee>
2022-09-15 19:46:02 -0300

    sql connection

--- Yana/Models/DomainEntities/YanaContext.cs
+++ Yana/Models/DomainEntities/YanaContext.cs
@@ -53,8 +53,11 @@ namespace Yana.Models.DomainEntities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                // Uncomment this to make it work on mac
                 optionsBuilder.UseSqlServer("Database=Yana;Trusted_Connection=False;User=sa;Password=Strong.Pwd-123;");
-                optionsBuilder.UseSqlServer("Database=Yana;Trusted_Connection=False;");
+
+                // Uncomment this to make it work on windows
+                // optionsBuilder.UseSqlServer("Database=Yana;Trusted_Connection=False;");
             }
         }
 
