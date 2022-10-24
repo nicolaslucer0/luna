4ff2df54d9057808f25a8ad7c75268c7eb91337f
Nicolas Lucero <nicolas.lucero@redb.ee>
2022-10-24 14:37:37 -0300

    translated datatables to spanish

---
+++ Yana/Controllers/StartingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Yana.BusinessLogic.Services;
+
+namespace Yana.Controllers
+{
+    public class StartingController
+    {
+        public StartingController()
+        {
+            this.UsuarioService = new UsuarioService();
+            this.PerfilService = new PerfilService();
+            this.PatologiaService = new PatologiaService();
+            this.PacientePatologiaService = new PacientePatologiaService();
+            this.NotificacionService = new NotificacionService();
+            this.ProfesionalPacienteService = new ProfesionalPacienteService();      
+    }
+        private UsuarioService UsuarioService { get; set; }
+        private PerfilService PerfilService { get; set; }
+        private PatologiaService PatologiaService { get; set; }
+        private PacientePatologiaService PacientePatologiaService { get; set; }
+        private NotificacionService NotificacionService { get; set; }
+        private ProfesionalPacienteService ProfesionalPacienteService { get; set; }
+    }
+}
+
